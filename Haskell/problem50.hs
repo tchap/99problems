68760354be98d1e82b8c194f659f2c2106bc18a2
Ondrej Kupka <ondra.cap@gmail.com>
2012-06-11 17:33:06 +0200

    Haskell: Problem 50

--- Haskell/problem50.hs
+++ Haskell/problem50.hs
@@ -3,47 +3,35 @@
 import Data.List
 import Data.Ratio
 
-data Tree v p = Node (v, p) (Tree v p) (Tree v p) | Empty deriving (Show, Eq)
+data Tree v p = Node (v, p) (Tree v p) (Tree v p) | Empty
+    deriving (Show, Eq)
 
-{- compare by priorities -}
+{- Empty never compared -}
 instance (Eq v, Ord p) => Ord (Tree v p) where
-    Empty `compare` Empty = EQ
-    Empty `compare` _ = LT
-    _ `compare` Empty = GT
-    (Node (_, pr1) _ _) `compare` (Node (_, pr2) _ _) = pr1 `compare` pr2
+    (Node (_, p1) _ _) `compare` (Node (_, p2) _ _) = p1 `compare` p2
 
-{- insert probabilities and continue -}
-huffman :: [(Char, Int)] -> [(Char, String)]
-huffman ps = huffman' rs
+huffman :: (Eq a) => [(a, Int)] -> [(a, String)]
+huffman ps = flatten $ encode "" $ toTree $ sort [Node k Empty Empty | k <- rs]
     where
         (rs, acc, _) = 
 	    let ff (rs, acc, w) (c, p) = ((c, p % w):rs, acc + p, w)
 	    in foldl ff ([], 0, acc) ps
 
-{- convert to a tree, insert codes, convert back -}
-huffman' :: [(Char, Ratio Int)] -> [(Char, String)]
-huffman' srs = flatten $ toCodeTree ht ""
-    where
-        ht = toProbTree $ sort [Node k Empty Empty | k <- srs]
-
-{- build the probability tree from a list of (symbol, probability) -}
-toProbTree :: [Tree Char (Ratio Int)] -> Tree Char (Ratio Int)
-toProbTree [t] = t
-toProbTree (t1:t2:ts) =
-    let Node (_, p1) _ _ = t1
+toTree :: (Eq a) => [Tree a (Ratio Int)] -> Tree a (Ratio Int)
+toTree [t] = t
+toTree (t1:t2:ts) =
+    let Node (v, p1) _ _ = t1
         Node (_, p2) _ _ = t2
-        t = Node ('x', p1 + p2) t1 t2
-    in toProbTree $ sort (t:ts)
+        t = Node (v, p1 + p2) t1 t2
+    in toTree $ sort (t:ts)
 
-{- convert a probability tree to the code tree -}
-toCodeTree :: Tree Char (Ratio Int) -> String -> Tree Char String
-toCodeTree Empty _ = Empty
-toCodeTree (Node (v, _) t1 t2) c =
-    let ct1 = toCodeTree t1 $ c ++ "0"
-	ct2 = toCodeTree t2 $ c ++ "1"
+encode :: String -> Tree a (Ratio Int) -> Tree a String
+encode _ Empty = Empty
+encode c (Node (v, _) t1 t2) =
+    let ct1 = encode (c ++ "0") t1
+	ct2 = encode (c ++ "1") t2
     in Node (v, c) ct1 ct2
 
-{- convert a tree back to a list -}
-flatten :: Tree Char String -> [(Char, String)]
+flatten :: Tree a String -> [(a, String)]
 flatten (Node k Empty Empty) = [k]
 flatten (Node _ t1 t2) = concatMap flatten [t1, t2]

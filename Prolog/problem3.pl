bca15a1154cbae8ce1561bc433ed40b954308f70
Ondrej Kupka <ondra.cap@gmail.com>
2012-06-11 22:25:57 +0200

    Prolog: improve Problem 3

--- Prolog/problem3.pl
+++ Prolog/problem3.pl
@@ -1,5 +1,6 @@
 %% 1.03 (*) Find the K'th element of a list.
 
+% replicate(+N, ?X, ?List)
 replicate(N, X, List) :- replicate(N, X, [], List).
 
 replicate(0, _, Acc, Acc) :- !. % green cut
@@ -8,9 +9,10 @@ replicate(N, X, Acc, List) :-
 	C is N - 1,
 	replicate(C, X, [X|Acc], List).
 
+% kth(+K, +List, ?E)
 kth(K, List, E) :- 
 	replicate(K, 1, C),
 	kth_acc(List, C, E).
 
-kth_acc([X|_], [_], X).
+kth_acc([X|_], [_], X) :- !. % green cut
 kth_acc([_|List], [_|C], K) :- kth_acc(List, C, K).

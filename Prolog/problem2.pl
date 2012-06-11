fead72921ebc22e479eb247b016ae807798ec89a
Ondrej Kupka <ondra.cap@gmail.com>
2012-06-11 22:12:38 +0200

    Prolog: improve Problem 2

--- Prolog/problem2.pl
+++ Prolog/problem2.pl
@@ -1,4 +1,5 @@
 %% 1.02 (*) Find the last but one element of a list.
 
-last_but_one([_,X], X) :- !.
+% last_but_one(+Xs, ?LBO)
+last_but_one([X,_], X) :- !.% green cut
 last_but_one([_|Xs], LBO) :- last_but_one(Xs, LBO).

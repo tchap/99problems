a788ffb6ccc3431d91164d176963d4415e6d6b98
Ondrej Kupka <ondra.cap@gmail.com>
2012-06-11 22:04:05 +0200

    Prolog: improve Problem 1

--- Prolog/problem1.pl
+++ Prolog/problem1.pl
@@ -1,4 +1,5 @@
 %% 1.01 (*) Find the last element of a list.
 
-last_elem([X], X) :- !.
-last_elem([_|Xs], Last) :- last_elem(Xs, Last).
+% last_elem(+Xs, ?Last)
+last_elem([X], X) :- !. % green cut
+last_elem([_|Xs], Last) :- [_|_] = Xs, last_elem(Xs, Last).

057e7c8f1d922d098170392982aa3910d6dad21f
Ondrej Kupka <ondra.cap@gmail.com>
2012-06-17 18:27:18 +0200

    Haskell: Problem 32

--- Haskell/problem32.hs
+++ Haskell/problem32.hs
@@ -1,5 +1,9 @@
 {- (**) Determine the greatest common divisor of two positive integer numbers.
          Use Euclid's algorithm. -}
 
-gcd' x 0 = x
-gcd' x y = gcd' y $ x `mod` y
+gcd' x y
+    | x >= y = gcd'' x y
+    | otherwise = gcd'' y x
+
+gcd'' x 0 = x
+gcd'' x y = gcd'' y $ x `mod` y

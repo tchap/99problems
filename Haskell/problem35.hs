90c62b2515edc53d4e8c1b0ae897f3dff9e70a29
Ondrej Kupka <ondra.cap@gmail.com>
2012-06-17 20:39:08 +0200

    Haskell: Problem 35: refactor

--- Haskell/problem35.hs
+++ Haskell/problem35.hs
@@ -2,10 +2,10 @@
        Construct a flat list containing the prime factors in ascending order.-}
 
 primeFactors :: Int -> [Int]
-primeFactors n = reverse $ primeFactors' n []
+primeFactors n = primeFactors' n []
 
 primeFactors' :: Int -> [Int] -> [Int]
-primeFactors' 1 ps = ps
+primeFactors' 1 ps = reverse ps
 primeFactors' n ps = primeFactors' q (k:ps)
     where k = head $ dropWhile (\x -> n `mod` x /= 0) [2,3..]
           q = n `div` k

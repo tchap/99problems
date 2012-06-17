fe24c0fbd8fd43c78fd5bf0921f6f448bd3728aa
Ondrej Kupka <ondra.cap@gmail.com>
2012-06-17 19:02:28 +0200

    Haskell: Problem 35 add types

--- Haskell/problem35.hs
+++ Haskell/problem35.hs
@@ -1,8 +1,10 @@
 {-(**) Determine the prime factors of a given positive integer. 
        Construct a flat list containing the prime factors in ascending order.-}
 
+primeFactors :: Int -> [Int]
 primeFactors n = reverse $ primeFactors' n []
 
+primeFactors' :: Int -> [Int] -> [Int]
 primeFactors' 1 ps = ps
 primeFactors' n ps = primeFactors' q (k:ps)
     where k = head $ dropWhile (\x -> n `mod` x /= 0) [2,3..]
